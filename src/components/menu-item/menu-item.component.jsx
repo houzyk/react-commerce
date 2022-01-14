d717e69084ece1a21d04c8d303d5db4102176176
Houzair Koussa <hkoussa@PBIO-2724s-MacBook-Pro.local>
2022-01-14 16:25:47 +0400

    ADD menu item component + sass

---
+++ src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+// css
+import './menu-item.styles.scss';
+
+const MenuItem = ({ title }) => {
+    <div className="menu-item">
+
+    </div>
+}
+
+export default MenuItem;

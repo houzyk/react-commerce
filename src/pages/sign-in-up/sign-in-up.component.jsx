e0f15cab52f32bdf974993a254adf18b342ba7d6
houzyk <houzairmk@icloud.com>
2022-01-17 10:00:58 +0400

    ADD form input styles

--- src/pages/sign-in-up/sign-in-up.component.jsx
+++ src/pages/sign-in-up/sign-in-up.component.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 
+// components
+import SignIn from "../../components/sign-in/sign-in.component";
+
 // css
 import './sign-in-up.styles.scss';
 
 const SignInUp = () => {
     return (
         <div className="sign-in-and-sign-up">
-            
+            <SignIn />
         </div>
     );
 }

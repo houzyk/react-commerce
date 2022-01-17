a49ebc0481cbeb1a61ec2796c080117215740845
houzyk <houzairmk@icloud.com>
2022-01-17 09:04:47 +0400

    ADD shop page style

--- src/components/preview-collection/preview-collection.component.jsx
+++ src/components/preview-collection/preview-collection.component.jsx
@@ -1,17 +1,16 @@
 import React from "react";
+import CollectionItem from "../collection-item/collection-item.component";
 
 // css
 import './preview-collection.styles.scss';
 
 const PreviewCollection = ({ title, items }) => {
 	return (
-		<div className="collection">
+		<div className="collection-preview">
 			<h1 className="title">{title}</h1>
 			<div className="preview">
-				{items.filter((i, index) => index < 4).map(item => (
-					<div key={item.id}>
-						{item.name}
-					</div>
+				{items.filter((i, index) => index < 4).map(({id, ...other}) => (
+					<CollectionItem key={id} {...other}/>
 				))
 				}	
 			</div>

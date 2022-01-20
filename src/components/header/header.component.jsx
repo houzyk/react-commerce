7d13dad937e31ea88d96614a1046865c5cd1684a
houzairkoussa <houzairmk@icloud.com>
2022-01-20 16:20:30 +0400

    ADD npm redux react-redux redux-logger

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -11,48 +11,48 @@ import './header.styles.scss';
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 
 const Header = ({ currentUser }) => {
-    return (
-        <div className="header">
-            <Link to="/" className="logo-container">
-                <Logo className="logo"/>
-            </Link>
+  return (
+    <div className="header">
+      <Link to="/" className="logo-container">
+          <Logo className="logo"/>
+      </Link>
+      <div
+        className="options"
+      >
+          <Link
+            to="/shop"
+            className="option"
+          >
+              SHOP
+          </Link>
+          <Link
+            to="/shop"
+            className="option"
+          >
+            CONTACT
+          </Link>
+          {
+            currentUser ?
+            (
             <div
-              className="options"
+              className="option"
+              onClick={() => auth.signOut()}
             >
-                <Link
-                  to="/shop"
-                  className="option"
-                >
-                    SHOP
-                </Link>
-                <Link
-                  to="/shop"
-                  className="option"
-                >
-                  CONTACT
-                </Link>
-                {
-                  currentUser ?
-                  (
-                  <div
-                    className="option"
-                    onClick={() => auth.signOut()}
-                  >
-                    SIGN OUT
-                  </div>
-                  )
-                  :
-                  (
-                  <Link
-                    className="option"
-                    to="/signin">
-                    SIGN IN
-                  </Link>
-                  )
-                }
+              SIGN OUT
             </div>
+            )
+            :
+            (
+            <Link
+              className="option"
+              to="/signin">
+              SIGN IN
+            </Link>
+            )
+          }
         </div>
-    );
+    </div>
+  );
 }
 
 export default Header;

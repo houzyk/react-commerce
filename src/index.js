871e95e781eae6232fcf601fbdae252a91314825
houzairkoussa <houzairmk@icloud.com>
2022-01-20 16:40:21 +0400

    ADD redux store into provider

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, } from "react-router-dom";
 
 // redux
 import { Provider } from "react-redux";
+import store from './redux/store';
 
 // components
 import App from './App';
@@ -13,7 +14,7 @@ import './index.css';
 
 
 ReactDOM.render(
-  <Provider>
+  <Provider store={store}>
     <Router>
       <App />
     </Router>

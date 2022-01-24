a6086619f907e15a6ad2d837fdeb9430848b3980
houzairkoussa <houzairmk@icloud.com>
2022-01-24 13:27:24 +0400

    ADD checkout page functionality

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -16,3 +16,8 @@ export const selectCartItemsCount = createSelector(
   [selectCartItems],
   (cartItems) => cartItems.reduce((qty, i) => qty + i.quantity, 0)
 )
+
+export const selectCartTotal = createSelector(
+  [selectCartItems],
+  (cartItems) => cartItems.reduce((qty, i) => qty + i.quantity * i.price, 0)
+)

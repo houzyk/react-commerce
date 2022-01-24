578bc5d17cb12f8e06191711b84cf8628ec70822
houzykhangar <houzair.koussa@hangarww.com>
2022-01-24 12:33:07 +0400

    ADD reselect for cart and current user

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,7 +3,9 @@ import { Link } from "react-router-dom";
 
 // redux
 import { connect } from "react-redux";
-
+import { createStructuredSelector } from "reselect";
+import { selectCartHidden } from "../../redux/cart/cart.selectors";
+import { selectCurrentUser } from "../../redux/user/user.selector";
 // firebase
 import { auth } from "../../firebase/firebase.utils";
 
@@ -67,9 +69,9 @@ const Header = ({ currentUser, hidden }) => {
   );
 }
 
-const mapStateToProps = ({user: {currentUser}, cart: { hidden }}) => ({
-  currentUser,
-  hidden
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser,
+  hidden: selectCartHidden
 })
 
 export default connect(mapStateToProps)(Header);

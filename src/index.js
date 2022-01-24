8fce34cf473ab1fcc34b9bde2f5f4e92991954ba
houzairkoussa <houzairmk@icloud.com>
2022-01-24 16:24:25 +0400

    ADD redux persist

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ import { BrowserRouter as Router, } from "react-router-dom";
 
 // redux
 import { Provider } from "react-redux";
-import store from './redux/store';
+import { store, persistor } from './redux/store';
+
+import { PersistGate } from "redux-persist/integration/react";
 
 // components
 import App from './App';
@@ -16,7 +18,9 @@ import './index.css';
 ReactDOM.render(
   <Provider store={store}>
     <Router>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </Router>
   </Provider>,
   document.getElementById('root')
